deea3bb5838024f7891084a2b120518f108c7a34
mikhael324 <83812749+mikhael324@users.noreply.github.com>
2023-07-21 10:40:27 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/mikhael324/Nairobi-koyeb /Nairobi
+  git clone https://github.com/mikhael324/Nairobi/Koyeb /Nairobi
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
